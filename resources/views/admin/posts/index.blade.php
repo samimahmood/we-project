b4a002ab65b5284d0be843c32f81d6fb5b800b97
sami mahmood <sm.ams37@gmail.com>
2017-06-06 10:09:01 +0500

    Post table in Dashboard Showing now with Categories From Db

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -28,7 +28,7 @@
                 <tr>
                     <td>{{$post->id}}</td>
                     <td>{{$post->user->name}}</td>
-                    <td>{{$post->category_id}}</td>
+                    <td>{{$post->category ? $post->category->name : "Not Categorized"}}</td>
                     <td><img height="50" src="{{$post->photo ? $post->photo->file : '/images/placeholder.jpg'}}" alt=""></td>
                     <td>{{$post->title}}</td>
                     <td>{{$post->body}}</td>
